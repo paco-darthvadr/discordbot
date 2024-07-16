e12a0d1f4480bcf077b96cc202b0ac51846593b5
paco-darthvadr <81834731+paco-darthvadr@users.noreply.github.com>
2024-07-16 10:22:38 +0100

    Update getdeeplink.js

--- utils/getdeeplink.js
+++ utils/getdeeplink.js
@@ -2,9 +2,10 @@ const  { VerusIdInterface, primitives } = require('verusid-ts-client');
 const { randomBytes } = require('crypto');
 const { I_ADDR_VERSION } = require('verus-typescript-primitives/dist/constants/vdxf.js');
 const axios = require('axios');
+require('dotenv').config();
 
-const VERUS_RPC_NETWORK = process.env.TESTNET == 'true' ? process.env.TESTNET_VERUS_RPC_NETWORK : process.env.MAINNET_VERUS_RPC_NETWORK
-const VERUS_RPC_SYSTEM = process.env.TESTNET == 'true' ? "VRSCTEST" : "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV";
+const VERUS_RPC_NETWORK = process.env.TESTNET == 'true' ? process.env.TESTNET_VERUS_RPC_NETWORK : process.env.TESTNET_VERUS_RPC_NETWORK 
+const VERUS_RPC_SYSTEM = process.env.TESTNET == 'true' ? "iJhCezBExJHvtyH3fGhNnt2NhU4Ztkf2yq" : "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV";
 const VerusId = new VerusIdInterface(VERUS_RPC_SYSTEM, VERUS_RPC_NETWORK);
 
 function generateChallengeID(len = 20) {
@@ -14,13 +15,13 @@ function generateChallengeID(len = 20) {
   return iaddress
 }
 
-const VALUID_IADDRESS = process.env.TESTNET == 'true' ? process.env.TESTNET_VALUID_IADDRESS : process.env.MAINNET_VALUID_IADDRESS
+
 const VALU_LOGIN_IADDRESS = process.env.TESTNET == 'true' ? process.env.TESTNET_VALU_LOGIN_IADDRESS : process.env.MAINNET_VALU_LOGIN_IADDRESS
-const PROVISIONING_URL = process.env.TESTNET == 'true' ? process.env.TESTNET_PROVISIONING_URL : process.env.MAINNET_PROVISIONING_URL
-const SYSTEM_ID = process.env.TESTNET == 'true' ? "iJhCezBExJHvtyH3fGhNnt2NhU4Ztkf2yq" : "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV";
+const LOGIN_URL = process.env.TESTNET_LOGIN_URL
 const VALU_LOGIN_WIF = process.env.TESTNET == 'true' ? process.env.TESTNET_VALU_LOGIN_WIF : process.env.MAINNET_VALU_LOGIN_WIF
-// PROVISION VALUID DEEPLINK
-const getdeeplink = async () => {
+
+// Login DEEPLINK
+const getdeeplink = async (userid) => {
 
   try {
     const challenge_id = generateChallengeID();
@@ -33,39 +34,17 @@ const getdeeplink = async () => {
           new primitives.RequestedPermission(primitives.IDENTITY_VIEW.vdxfid),
         ],
         redirect_uris: [ new primitives.RedirectUri(
-          `${PROVISIONING_URL}/verusidloginnewaccount`, 
+          `${LOGIN_URL}/registerdiscorduser?id=${userid}`, 
           primitives.LOGIN_CONSENT_WEBHOOK_VDXF_KEY.vdxfid
         ),
-        ],
-        subject: [
-          new primitives.Subject(
-            SYSTEM_ID,
-            primitives.ID_SYSTEMID_VDXF_KEY.vdxfid
-          ),
-          new primitives.Subject(
-            VALUID_IADDRESS,
-            primitives.ID_PARENT_VDXF_KEY.vdxfid
-          ),
-        ],
-        provisioning_info: [
-          new primitives.ProvisioningInfo(
-            `${PROVISIONING_URL}/provisionVerusId`,
-            primitives.LOGIN_CONSENT_ID_PROVISIONING_WEBHOOK_VDXF_KEY.vdxfid
-          ),
-          new primitives.ProvisioningInfo(
-            SYSTEM_ID,
-            primitives.ID_SYSTEMID_VDXF_KEY.vdxfid
-          ),
-          new primitives.ProvisioningInfo(
-            VALUID_IADDRESS,  //valuID@ testnet
-            primitives.ID_PARENT_VDXF_KEY.vdxfid
-          ),
         ],
         created_at: Number((Date.now() / 1000).toFixed(0)),
       }),
       VALU_LOGIN_WIF
     );
 
+    console.log(response.toWalletDeeplinkUri())
+    console.log(response.challenge.redirect_uris)
     return response.toWalletDeeplinkUri();
     
   } catch (e) {
