40c1080df0c85c0978d3beefffe21d7bc03f3778
paco-darthvadr <81834731+paco-darthvadr@users.noreply.github.com>
2024-07-16 10:26:18 +0100

    Update getid.js

--- commands/utility/getid.js
+++ commands/utility/getid.js
@@ -4,40 +4,61 @@ const { checkDiscordUserExists, setDiscordUsers } = require("../../utils/databas
 const wait = require('node:timers/promises').setTimeout;
 const qr = require('qr-image');
 const fs = require('fs');
+require('dotenv').config();
+
+
+
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('getid')
-		.setDescription('Get a ValuID'),
+	  .setName('getid')
+	  .setDescription('Get Verified'),
 	async execute(interaction) {
-
-		if (checkDiscordUserExists(interaction.user.id)) {
-			return await interaction.reply({ content: "You have already requested a ValuID.",  ephemeral: true });
+	  try {
+		const userid = interaction.user.id;
+  
+		if (checkDiscordUserExists(userid)) {
+		  return await interaction.reply({ content: "You have already verified.", ephemeral: true });
 		}
-		
+  
 		await interaction.deferReply({ ephemeral: true });
-		const deeplinkurl = await getdeeplink();
+  
+		const deeplinkurl = await getdeeplink(userid);
 		const tinyurl = await getTinyUrl(deeplinkurl);
-		var qr_png = qr.image(deeplinkurl, { type: 'png' });
-		const pngname = `${interaction.user.id}qr_code.png`;
-		await qr_png.pipe(fs.createWriteStream("./public/" + pngname));
-		await wait(2_000);
-		const file = new AttachmentBuilder("./public/" + pngname);
-		
-		await interaction.editReply({ embeds: [templateEmbed(pngname, tinyurl)], files: [file] });
+  
+		const qr_png = qr.image(deeplinkurl, { type: 'png' });
+		const pngname = `${interaction.user.id}_qr_code.png`;
+  
+		await qr_png.pipe(fs.createWriteStream(`./public/${pngname}`));
+		await wait(2000);
+  
+		const file = new AttachmentBuilder(`./public/${pngname}`);
+  
+		await interaction.editReply({
+		  embeds: [templateEmbed(pngname, tinyurl)],
+		  files: [file],
+		});
+  
 		setDiscordUsers(interaction.user.id);
+  
+	  } catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while processing your request.', ephemeral: true });
+	  }
 	},
-};
+  };
+  
 
-const templateEmbed = (pngname, tinylink) => {
+  const templateEmbed = (pngname, tinylink) => {
 	return new EmbedBuilder()
 	.setColor(0x0099FF)
-	.setTitle('Your ValuID Link')
+	.setTitle('Your Verus Verify Login Link')
 	.setURL(tinylink)
-	.setAuthor({ name: 'Valu', iconURL: 'https://pbs.twimg.com/profile_images/1499065754313887747/_YAZWd_X_400x400.jpg', url: 'https://valu.earth' })
-	.setDescription('Please click on the link in a mobile device to open the Verus Mobile app, or scan the QR code to create a ValuID@')
-	.setThumbnail('https://pbs.twimg.com/profile_images/1499065754313887747/_YAZWd_X_400x400.jpg')
+	.setAuthor({ name: 'Verus', iconURL: 'https://cdn.discordapp.com/emojis/1239220413578350603.png', url: 'https://discord.com' })
+	.setDescription('Please click on the link in a mobile device to open the Verus Mobile app, or scan the QR code to verify')
+	.setThumbnail('https://cdn.discordapp.com/emojis/1239220413578350603.png')
 	.setImage(`attachment://${pngname}`)
 	.setTimestamp()
-	.setFooter({ text: 'Sent from Valu', iconURL: 'https://pbs.twimg.com/profile_images/1499065754313887747/_YAZWd_X_400x400.jpg' })
+	.setFooter({ text: 'Sent from Verus', iconURL: 'https://cdn.discordapp.com/emojis/1239220413578350603.png' });
 };
